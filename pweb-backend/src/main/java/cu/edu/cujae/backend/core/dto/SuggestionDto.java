2d70465736b65256c26f36f2493609dfa9f82ef9
Leocujae <leonardocujae@gmail.com>
2024-01-05 17:41:27 -0500

    SuggestionDto update

--- pweb-backend/src/main/java/cu/edu/cujae/backend/core/dto/SuggestionDto.java
+++ pweb-backend/src/main/java/cu/edu/cujae/backend/core/dto/SuggestionDto.java
@@ -9,15 +9,15 @@ public class SuggestionDto {
 	private String created_on;
 	
 	private int author_id;
-
-	private String description;
+	private String subjet;
+	private String text;
 	private String urgency;
 	private String importance;
-	public SuggestionDto(int id, int author_id, String description, String created_on, String urgency, String importance ) {
+	public SuggestionDto(int id, int author_id, String description, String created_on, String urgency, String importance , String subject) {
 		super();
 		this.id = id;
 		this.author_id = author_id;
-		this.description = description;
+		this.text = description;
 		this.created_on = created_on;
 		this.urgency = urgency;
 		this.importance = importance;
@@ -37,10 +37,10 @@ public class SuggestionDto {
 		this.author_id = author_id;
 	}
 	public String getDescription() {
-		return description;
+		return text;
 	}
 	public void setDescription(String description) {
-		this.description = description;
+		this.text = description;
 	}
 	public String getCreated_on() {
 		return created_on;
@@ -59,6 +59,18 @@ public class SuggestionDto {
 	}
 	public void setImportance(String importance) {
 		this.importance = importance;
+	}
+	public String getSubjet() {
+		return subjet;
+	}
+	public void setSubjet(String subjet) {
+		this.subjet = subjet;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
 	}
 	
 }
